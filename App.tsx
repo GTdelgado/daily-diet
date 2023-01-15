f296bdb313a65849bef2897437152ee8e1710929
gtdelgado <delgado.gabrielteixeira00@gmail.com>
2023-01-15 18:28:20 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import { useFonts, Nunito_400Regular, Nunito_700Bold } from '@expo-google-fonts/nunito'
+import theme from './src/theme';
+import { Loading } from '@components/Loading';
 
 export default function App() {
+  const [fonsLoaded] = useFonts({
+    Nunito_400Regular,
+    Nunito_700Bold
+  })
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <ThemeProvider theme={theme}>
       <StatusBar style="auto" />
-    </View>
+
+      {fonsLoaded ?
+        <Text>Open up App.tsx to start working on your app!</Text>
+        :
+        <Loading />}
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
